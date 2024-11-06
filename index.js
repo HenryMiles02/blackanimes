59fe20115d0f9b849beea7a6dd8ba17279a1a6ff
HenryMiles02 <rosacarlos953@gmail.com>
2024-11-06 14:27:26 -0300

    novo carrossel

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ var card5 = `
                             <p>3 Temporadas</p>
                         </div>
                     </div>
-                    <p>
+                    <p class="text">
                         Em um futuro próximo, foi lançado um Jogo de Realidade Virtual em Massa para Múltiplos Jogadores Online (VRMMORPG) chamado Sword Art Online, onde seus jogadores controlam seus personagens com o próprio corpo usando um dispositivo tecnológico chamado: NerveGear. Um dia, os jogadores descobrem que não podem sair do jogo, pois o criador do jogo os mantêm presos a menos que eles cheguem ao 100º andar da Torre e derrotem o Boss final. No entanto, se eles morrerem no jogo, morrerão também na vida real.
                     </p>
                     <div class="card-buttons">
