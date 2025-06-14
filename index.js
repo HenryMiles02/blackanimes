d105bfb5eaf390e21c2e95fef9571cb025a42ae2
Erik Vidal <erikvidaldesouza2@gmail.com>
2025-06-14 15:41:41 -0300

    refatoração do projeto

--- index.js
+++ index.js
@@ -2,204 +2,143 @@
 
 const carrossel = document.getElementById("carrossel1")
 
-var card1 = `
-<div class="container-carrossel" id="img1">
-    <div class="slide">
-        <button class="btn-slide" onclick="beforeButton()"> &lt; </button>
-        <img src="img/banner-jujutsu-carrosse.jfif" alt="" class="bg-carrosel" id="imgContainer">
-        <!--"img/banner-jujutsu-carrosse.jfif", "img/banner-rimuro-carrossel.jpg", "img/banner-kimetsu-carrossel.jfif"-->
-        <button class="btn-slide" onclick="nextButton()"> &gt; </button>
-    </div>
+const cards= [
+    {
+        title: "Jujutsu Kaisen",
+        img: "img/banner-jujutsu-carrosse.jfif",
+        age_rating: "A16",
+        seasons: 3,
+        description: "Yuji Itadori, um estudante do ensino médio que se envolve no mundo das maldições após ingerir um poderoso objeto amaldiçoado: o dedo do espírito maligno Ryomen Sukuna. Com a ajuda de outros exorcistas, como Megumi Fushiguro e Nobara Kugisaki, Yuji luta contra seres sobrenaturais enquanto busca uma maneira de controlar Sukuna e proteger seus amigos.",
+    },
+    {
+      title: "Kimetsu no Yaiba",
+      img: "img/banner-kimetsu-carrossel.jfif",
+      age_rating: "A16",
+      seasons: 3,
+      description: "Tanjiro Kamado é um jovem que busca vingar sua família, assassinada por demônios, e salvar sua irmã Nezuko, que foi transformada em um deles. Ele se junta aos Caçadores de Demônios para lutar contra essas criaturas e poder salvar sua irmã.",
+    },
+    {
+        title: "One Piece",
+        img: "img/banner-one-piece-carrossel.jpg",
+        age_rating: "A14",
+        seasons: "11 Temporadas",
+        description: "One Piece é um anime que segue as aventuras de Monkey D. Luffy, um jovem pirata que sonha em se tornar o Rei dos Piratas. Com a habilidade de esticar seu corpo como borracha, Luffy forma a tripulação dos Chapéus de Palha e embarca em uma jornada pela Grand Line em busca do lendário tesouro 'One Piece'.",
+    },
+    {
+        title: "That Time I Got Reincarnated as a Slime",
+        img: "img/banner-rimuro-carrossel.jpg",
+        age_rating: "A14",
+        seasons: 3,
+        description: "Satoru Mikami, um homem comum que, após ser morto, reencarna em um mundo de fantasia como um slime. Com habilidades únicas, ele ganha o nome de Rimuru Tempest e começa a construir um novo lar para monstros, buscando a paz e a prosperidade. Rimuru forma alianças com diversas espécies e enfrenta poderosos inimigos, fazendo novas amizades, obtendo poder e sempre vivendo uma aventura.",
+    },
+    {
+        title: "Sword Art Online",
+        img: "img/banner-sao-carrossel.jpg",
+        age_rating: "A14",
+        seasons: 3,
+        description: "Em um futuro próximo, foi lançado um Jogo de Realidade Virtual em Massa para Múltiplos Jogadores Online (VRMMORPG) chamado Sword Art Online, onde seus jogadores controlam seus personagens com o próprio corpo usando um dispositivo tecnológico chamado: NerveGear. Um dia, os jogadores descobrem que não podem sair do jogo, pois o criador do jogo os mantêm presos a menos que eles cheguem ao 100º andar da Torre e derrotem o Boss final. No entanto, se eles morrerem no jogo, morrerão também na vida real.",
+    },
+]
 
-    <div class="slide-card">
-        <div class="descricao">
-            <h1>Jujutsu Kaisen</h1>
+let indice = 0;
+
+function renderCard(index) {
+    const {title, img, age_rating, seasons, description} = cards[index];
+
+    carrossel.innerHTML = `
+      <div class="container-carrossel">
+        <div class="slide">
+          <button class="btn-slide" onclick="beforeButton()"> &lt; </button>
+          <img src="${img}" alt="" class="bg-carrosel">
+          <button class="btn-slide" onclick="nextButton()"> &gt; </button>
+        </div>
+        <div class="slide-card">
+          <div class="descricao">
+            <h1>${title}</h1>
             <div class="temp-class">
-                <img src="img/icons/A16.jpg" alt="">
-                <p>3 Temporadas</p>
+              <img src="img/icons/${age_rating}.jpg" alt="">
+              <p>${seasons} Temporada</p>
             </div>
+          </div>
+          <p class="text">${description}</p>
+          <div class="card-buttons">
+            <button>Assistir agora</button>
+          </div>
         </div>
-        <p class="text">
-            Yuji Itadori, um estudante do ensino médio que se envolve no mundo das maldições após ingerir um
-            poderoso objeto amaldiçoado: o dedo do espírito maligno Ryomen Sukuna. Com a ajuda de outros exorcistas,
-            como Megumi Fushiguro e Nobara Kugisaki, Yuji luta contra seres sobrenaturais enquanto busca uma maneira
-            de controlar Sukuna e proteger seus amigos.
-        </p>
-        <div class="card-buttons">
-            <button>Assitir agora</button>
-                        <!--<a href="#" class="favorite"></a>   botão de favoritos -->
-        </div>
-    </div>
-</div> 
-`
-var card2 = `
-<div class="container-carrossel" id="card2">
-                <div class="slide">
-                    <button class="btn-slide" onclick="beforeButton()"> &lt; </button>
-                    <img src="img/banner-kimetsu-carrossel.jfif" alt="" class="bg-carrosel">
-                    <button class="btn-slide"onclick="nextButton()"> &gt; </button>
-                </div>
-                
-                <div class="slide-card">
-                    <div class="descricao">
-                        <h1>Kimetsu no Yaiba</h1>
-                        <div class="temp-class">
-                            <img src="img/icons/A16.jpg" alt="">
-                            <p>3 Temporadas</p>
-                        </div>
-                    </div>
-                    <p class="text">
-                        Tanjiro Kamado é um jovem que busca vingar sua família, assassinada por demônios, e salvar sua irmã Nezuko, que foi transformada em um deles. Ele se junta aos Caçadores de Demônios para lutar contra essas criaturas e poder salvar sua irmã.
-                    </p>
-                    <div class="card-buttons">
-                        <button>Assitir agora</button>
-                        <!--<a href="#" class="favorite"></a>   botão de favoritos -->
-                    </div>
-                </div>
-            </div>
-`
-var card3 = `
-<div class="container-carrossel" id="card3">
-                <div class="slide">
-                    <button class="btn-slide" onclick="beforeButton()"> &lt; </button>
-                    <img src="img/banner-one-piece-carrossel.jpg" alt="" class="bg-carrosel">
-                    <button class="btn-slide" onclick="nextButton()"> &gt; </button>
-                </div>
-                
-                <div class="slide-card">
-                    <div class="descricao">
-                        <h1>One Piece</h1>
-                        <div class="temp-class">
-                            <img src="img/icons/A14.jpg" alt="">
-                            <p>11 Temporadas</p>
-                        </div>
-                    </div>
-                    <p class="text">
-                        One Piece é um anime que segue as aventuras de Monkey D. Luffy, um jovem pirata que sonha em se tornar o Rei dos Piratas. Com a habilidade de esticar seu corpo como borracha, Luffy forma a tripulação dos Chapéus de Palha e embarca em uma jornada pela Grand Line em busca do lendário tesouro "One Piece".
-                    </p>
-                    <div class="card-buttons">
-                        <button><a href="#">Assitir agora</a></button>
-                        <!--<a href="#" class="favorite"></a>   botão de favoritos -->
-                    </div>
-                </div>
-            </div>
-`
-var card4 = `
-<div class="container-carrossel" id="card4">
-                <div class="slide">
-                    <button class="btn-slide" onclick="beforeButton()"> &lt; </button>
-                    <img src="img/banner-rimuro-carrossel.jpg" alt="" class="bg-carrosel">
-                    <button class="btn-slide" onclick="nextButton()"> &gt; </button>
-                </div>
-                
-                <div class="slide-card">
-                    <div class="descricao">
-                        <h1>That Time I Got Reincarnated as a Slime</h1>
-                        <div class="temp-class">
-                            <img src="img/icons/A14.jpg" alt="">
-                            <p>3 Temporadas</p>
-                        </div>
-                    </div>
-                    <p class="text">
-                        Satoru Mikami, um homem comum que, após ser morto, reencarna em um mundo de fantasia como um slime. Com habilidades únicas, ele ganha o nome de Rimuru Tempest e começa a construir um novo lar para monstros, buscando a paz e a prosperidade. Rimuru forma alianças com diversas espécies e enfrenta poderosos inimigos, fazendo novas amizades, obtendo poder e sempre vivendo uma aventura.
-                    </p>
-                    <div class="card-buttons">
-                        <button>Assitir agora</button>
-                        <!--<a href="#" class="favorite"></a>   botão de favoritos -->
-                    </div>
-                </div>
-            </div>
-`
-var card5 = `
-<div class="container-carrossel" id="card5">
-                <div class="slide">
-                    <button class="btn-slide" onclick="beforeButton()"> &lt; </button>
-                    <img src="img/banner-sao-carrossel.jpg" alt="" class="bg-carrosel">
-                    <button class="btn-slide" onclick="nextButton()"> &gt; </button>
-                </div>
-                
-                <div class="slide-card">
-                    <div class="descricao">
-                        <h1>Sworld Art Oline</h1>
-                        <div class="temp-class">
-                            <img src="img/icons/A14.jpg" alt="">
-                            <p>3 Temporadas</p>
-                        </div>
-                    </div>
-                    <p class="text">
-                        Em um futuro próximo, foi lançado um Jogo de Realidade Virtual em Massa para Múltiplos Jogadores Online (VRMMORPG) chamado Sword Art Online, onde seus jogadores controlam seus personagens com o próprio corpo usando um dispositivo tecnológico chamado: NerveGear. Um dia, os jogadores descobrem que não podem sair do jogo, pois o criador do jogo os mantêm presos a menos que eles cheguem ao 100º andar da Torre e derrotem o Boss final. No entanto, se eles morrerem no jogo, morrerão também na vida real.
-                    </p>
-                    <div class="card-buttons">
-                        <button>Assitir agora</button>
-                        <!--<a href="#" class="favorite"></a>   botão de favoritos -->
-                    </div>
-                </div>
-            </div>
-`
-
-lista = [card1, card2, card3, card4, card5]
-
-indice = 0
-
-function nextButton() {
-    indice ++;
-    if (indice >= lista.length) {
-        indice = 0;
-    }
-    carrossel.innerHTML = lista[indice];
+      </div>`;
 }
 
-function beforeButton() {
-    indice--;
-    if (indice < 0) {
-        indice = lista.length -1;
-    }
-    carrossel.innerHTML = lista[indice];
+window.nextButton = function () {
+    indice = (indice + 1) % cards.length;
+    renderCard(indice);
+};
+
+ window.beforeButton = function() {
+    indice = (indice - 1 + cards.length) % cards.length;
+    renderCard(indice);
 }
+
+renderCard(indice);
 
     //carrossel pequeno
 
 const swiper = new Swiper('.card-wrapper', {
-    loop: true,
-    spaceBetween: 20,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  loop: true,
+  lazyPreloadPrevNext: 2,
+  preloadImages: false,
+  lazy: {
+    loadPrevNext: true,
+  },
+  spaceBetween: 16,
 
-    breakpoints: {
-       0: {
-        slidesPerView: 1
-       },
-       760: {
-        slidesPerView: 2
-       },
-       1024: {
-        slidesPerView: 6
-       },
+  slidesPerView: 1.3, // para exibir parte do próximo card em telas menores
+  centeredSlides: true,
+  grabCursor: true,
+  speed: 500,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 2.5,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    900: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+    1280: {
+      slidesPerView: 6,
     }
-  });
-
+  }
+});
   //timer epsodios
 
   
   const minutos = document.getElementById("relogio");
+  var dataDePostagem = new Date('2024-11-05T17:49:00')
   
-  const relogio = setInterval(function time() {
-    var dataDePostagem = new Date('2024-11-05T17:49:00')
+  setInterval(() => {
 
-    let dateToday = new Date();
-
+    const dateToday = new Date();
     const diferenca = dateToday - dataDePostagem; 
+
 
     const s = Math.floor(diferenca / 1000);
     const min = Math.floor(s / 60);
@@ -208,29 +147,21 @@ const swiper = new Swiper('.card-wrapper', {
     const months = Math.floor(days / 30); // Considera um mês como 30 dias
     const years = Math.floor(months / 12);
 
-    if(min <= 1){
-        minutos.innerHTML = `<span style="color: aqua; font-size:24px;" class="" id="number"> ${min} </span>
-        <p>minuto</p>`
-    }
-    if(min >= 2){
-        minutos.textContent = `<span style="color: aqua; font-size:24px;" class="" id="number"> ${min} </span>`
-    }
-    if(min > 59){
-        minutos.textContent = `<span style="color: aqua; font-size:24px;" class="" id="number"> ${hr} </span>`
-    }
-    if (hr >= 2){
-        minutos.textContent = `<span style="color: aqua; font-size:24px;" class="" id="number"> ${hr} </span> horas`
-    }
-    if (hr > 23){
-        minutos.textContent = `<span style="color: aqua; font-size:24px;" class="" id="number"> ${days} </span> dia`
-    }
-    if (days >= 2){
-        minutos.innerHTML = `<span style="color: aqua;" class="" id="number"> ${days} </span> dias`
-    }
-    if (days > 30){
-        minutos.textContent = `<span style="color: aqua; font-size:24px;" class="" id="number"> ${months} </span>  mês`
-    }
-    if (months >= 2){
-        minutos.textContent = `<span style="color: aqua; font-size:24px;" class="" id="number"> ${months} </span>  meses`
-    }
-  }, 1000)
+  let texto = "";
+
+  if (min < 1) {
+    texto = `agora`;
+  } else if (min < 60) {
+    texto = `${min} minuto${min > 1 ? "s" : ""}`;
+  } else if (hr < 24) {
+    texto = `${hr} hora${hr > 1 ? "s" : ""}`;
+  } else if (days < 30) {
+    texto = `${days} dia${days > 1 ? "s" : ""}`;
+  } else if (months < 12) {
+    texto = `${months} mês${months > 1 ? "es" : ""}`;
+  } else {
+    texto = `${years} ano${years > 1 ? "s" : ""}`;
+  }
+
+  minutos.innerHTML = `<span style="color: aqua; font-size:24px;" id="number">${texto}</span>`;
+}, 1000);
